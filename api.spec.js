be3a9853bbfc7c75d3e4ddb181829fdc1f6c475a
mikhail <mikzerafa@hotmail.com>
2023-01-26 22:02:13 +0100

    started readme

--- api.spec.js
+++ api.spec.js
@@ -1,13 +1,18 @@
-import chai from 'chai';
-import 'mocha';
-import chaihttp from 'chai-http';
+const chai = require('chai');
+require('mocha');
+const chaihttp = require('chai-http');
+const je = require('json-evolve/types/jsonEvolve.js').default;
 
 chai.use(chaihttp);
 
 describe('Generic test', () => {
     it('Should return json from api endpoint', async() => {
-        const providers = await chai.request('https://backend.pixel.bet/rest/casino/providers')
+        const providers = await chai.request('https://backend.pixel.bet')
+            .get('/rest/casino/providers')
             .send()
+
+        //je.assert.KeyExists(providers.body, "title");
+        je.assert.KeyContainsData(providers.body, "title");
 
     })
 })
