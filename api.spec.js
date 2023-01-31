bf35ee6d8e36d5d3163ee51d2da94b789e43480d
mikhail <mikzerafa@hotmail.com>
2023-01-31 14:00:35 +0100

    fixed parsing on pixel/backend end by excluding array, added looped it block to backend

--- api.spec.js
+++ api.spec.js
@@ -3,16 +3,52 @@ require('mocha');
 const chaihttp = require('chai-http');
 const je = require('json-evolve/types/jsonEvolve.js').default;
 
+const keys = ["title", "key", "ordinal", "ageLimit"];
+const myIt = it;
+const myIt_Key = (json, key) => myIt("should have key: " + key + " which contains data", (json2 = json, key2 = key) => { testKey(json2, key2) })
+const myIt_Providers_Key = async(key) => myIt("should have key: " + key + " which contains data", async() => {
+    let providers = await getProviders();
+    providers = CleanJson(providers);
+    testKey(providers, key)
+})
+
 chai.use(chaihttp);
 
-describe('Generic test', () => {
-    it('Should return json from api endpoint', async() => {
-        const providers = await chai.request('https://backend.pixel.bet')
-            .get('/rest/casino/providers')
-            .send()
-
-        //je.assert.KeyExists(providers.body, "title");
-        je.assert.KeyContainsData(providers.body, "title");
-
+describe('Provider Test', async() => {
+    keys.forEach(key => {
+        myIt_Providers_Key(key);
     })
 })
+
+
+// const myIt = (title, param, func) => it(title, func(param));
+// const myIt_2 = (title, param1, param2, func) => it(title, (param1, param2) => {
+//     func(param1, param2);
+// });
+// const myIt_Key = (json, key) => myIt_2("", json, key, testKey);
+// // je.print(providersJson);
+
+async function getProviders() {
+    const providers = await chai.request('https://backend.pixel.bet')
+        .get('/rest/casino/providers')
+        .send()
+
+    return JSON.stringify(providers.body);
+}
+
+function CleanJson(json) {
+    if (json[0] == "[") {
+        return json.substring(1, json.length - 1);
+    }
+    return json;
+}
+
+function testKey(json, key) {
+    process.stdout.write("\n\nkey '" + key + "' Exists...");
+    je.assert.KeyExists(json, key);
+    console.log('Pass');
+
+    process.stdout.write("key '" + key + "' contains Data...");
+    je.assert.KeyContainsData(json, key);
+    console.log('Pass')
+}
